36d6dc7cc859e67d86cc8fc412e617b8087e3aa1
Justin Hoang <jhoang1996@utexas.edu>
2018-04-02 21:05:19 -0500

    Documentation of Google Maps Completed

---
+++ app/src/main/java/com/example/homework4/MapActivity.java
@@ -0,0 +1,16 @@
+package com.example.homework4;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MapActivity extends AppCompatActivity{
+
+    private static final String TAG = "MapActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
